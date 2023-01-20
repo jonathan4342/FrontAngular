e75de4034fb6eba42f5b040087b207810762f13f
Jonathan Osorio <jonathan.osorio@devitech.com.co>
2023-01-19 20:47:41 -0500

    [UPDATE] se realizo el form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
   ],
   imports: [
     BrowserModule,
@@ -18,7 +18,8 @@ import { HttpClientModule } from '@angular/common/http'
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import axios from 'axios';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
@@ -8,23 +9,51 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class FormularioComponent implements OnInit{
 
   miFormularioValidado:FormGroup=new FormGroup({})
-  
+  setModal:boolean=false;
+  message:Object={};
+  statusCode:Object={};
   constructor(private formBuilder:FormBuilder) { }
   
   ngOnInit(): void {
     this.miFormularioValidado=this.formBuilder.group({
       email:['',Validators.compose([Validators.required,Validators.email])],
-      password:['',Validators.compose([Validators.required,Validators.minLength(8),Validators.pattern('[a-zA-Z0-9]*')])],
+      password:['',Validators.compose([Validators.required,Validators.minLength(2),Validators.pattern('[a-zA-Z0-9]*')])],
     })
-
+    
   }
-  enviarFormulario(){
+
+  get email(){
+    return this.miFormularioValidado.get('email');
+  }
+  get password(){
+    return this.miFormularioValidado.get('password');
+  }
+    async enviarFormulario(){
+
     // Controlar que el formulario este validado
     if(this.miFormularioValidado.valid){
-      console.table(this.miFormularioValidado.value);
-      // Limpiar el formulario
+      axios.post('http://192.168.88.174/Login-Php/index.php',this.miFormularioValidado.value)
+      .then((response)=>{
+        localStorage.setItem('token',JSON.stringify(response.data));
+        this.setModal=true;
+        
+        if(localStorage.getItem('token')){
+          this.message = response.data.message;
+          this.statusCode = response.data.status;
+        }
+      })
+      .catch((error)=>{
+        console.log(error);
+        
+      })
       this.miFormularioValidado.reset();
     }
+    
+    
+  }
+  closeModal(){
+    this.setModal=false;
+    //localStorage.removeItem('token');
   }
 
 }
